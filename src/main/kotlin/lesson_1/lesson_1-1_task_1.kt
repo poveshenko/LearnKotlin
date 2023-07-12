3ee3373713cfb402b91205390c0b3f52b09322b8
Evgeniy <E.poveshenko@gmail.com>
2023-07-12 16:16:44 +0300

    LK-1-3 print/ Первый урок и первая задача/Исправление/ отступ

--- src/main/kotlin/lesson_1/lesson_1-1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1-1_task_1.kt
@@ -1,9 +1,9 @@
 package lesson_1
 
-fun main(){
+fun main() {
     val numberOfOrders = 75
     val text = " Thank you for your purchase! "
 
-println("The number of orders for all time: $numberOfOrders")
+    println("The number of orders for all time: $numberOfOrders")
     println(text)
 }
