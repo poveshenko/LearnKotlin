68450f7a36ceaa4b10af76b6b1a2d7119776663b
Evgeniy <E.poveshenko@gmail.com>
2023-07-12 17:25:33 +0300

    LK-1-3 print/ Первый урок и четвертая задача/Типы данных/

---
+++ src/main/kotlin/Lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Float = 2.0547945205479453E-4F
+    val apogee: Int = 327000
+}
+
+
