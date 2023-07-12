6d80888b7188148415b593ecf314a9275fdf97e0
Evgeniy <E.poveshenko@gmail.com>
2023-07-12 14:19:09 +0300

    LK-1-1 print/ Первый урок и первая задача/ Переменные/изменения

--- src/main/kotlin/Lesson_1/Lesson_1-1_task_1.kt
+++
@@ -1,9 +0,0 @@
-package Lesson_1
-
-fun main(){
-    val NumberOfOrders=75
-    val Text = " Thank you for your purchase! "
-
-println("The number of orders for all time: "+NumberOfOrders)
-    println(Text)
-}
