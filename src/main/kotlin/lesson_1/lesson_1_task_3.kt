8e4b515dddc2248c46dc9468fa9eb7dfa810fe23
Evgeniy <E.poveshenko@gmail.com>
2023-07-12 15:45:51 +0300

    LK-1-3 print/ Первый урок и третья задача/ Переменные/Гагарин

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+package lesson_1
+
+fun main(){
+    val year = 1991
+    var hour = "09"
+    var minute = "07"
+    println("Год полета: $year")
+
+    println("Взлет: $hour:$minute")
+
+
+    hour = "10"
+    minute = "55"
+
+
+    print("Посадка: $hour:$minute")
+
+
+
+}
