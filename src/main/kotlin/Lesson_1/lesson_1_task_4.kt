226802cefed7144c7ccd11cc60b1d263ecf72c2d
Evgeniy <E.poveshenko@gmail.com>
2023-07-13 17:49:48 +0300

    LK-1-4 print/ Первый урок и четвертая задача/Изменение

--- src/main/kotlin/Lesson_1/lesson_1_task_4.kt
+++ src/main/kotlin/Lesson_1/lesson_1_task_4.kt
@@ -7,6 +7,8 @@ fun main() {
     val seconds: Short = 6480
     val partOfYear: Float = 2.0547945205479453E-4F
     val apogee: Int = 327000
+    print(" Расстояние: $length\n Возраст: $age\n Часть дня: $partOfDay\n Секунды: $seconds\n Часть года: $partOfYear\n Апогей орбиты: $apogee")
+
 }
 
 
