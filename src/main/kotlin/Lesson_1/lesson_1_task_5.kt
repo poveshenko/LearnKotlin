7a0795a4b110b468c16613e255f22de1d5127abc
Evgeniy <E.poveshenko@gmail.com>
2023-07-13 18:31:32 +0300

    LK-1-5 print/ Первый урок и пятая задача/Изменение

--- src/main/kotlin/Lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/Lesson_1/lesson_1_task_5.kt
@@ -3,9 +3,9 @@ package lesson_1
 
 fun main() {
     val seconInSpace = 6480
-    val sec = seconInSpace % 60
     val min = seconInSpace / 60 % 60
     val hours = seconInSpace / 60 / 60
-    println("$hours:$min:$sec")
 
+    val time= String.format("%02d:%02d", hours, min, )
+    println(time)
 }
