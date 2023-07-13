4359f3b241cda3645259453a74eff2c36ddd0045
Evgeniy <E.poveshenko@gmail.com>
2023-07-13 22:03:34 +0300

    LK-2-4 print/ Второй урок и четвертая задача/

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,9 @@
+package lesson_2
+
+fun main() {
+    val buff = 20
+    val oreCrystal = buff % 7
+    val ironOre = buff % 11
+    println("Кристалическая Руда:  $oreCrystal% бонусных")
+    println("Железная Руда:  $ironOre% бонусных")
+}
