69d950361d18b0d7da3e2b445613a5ed92913ee4
Evgeniy <E.poveshenko@gmail.com>
2023-07-17 22:33:06 +0300

    LK-3-4 print/

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+    var moveNumber = 1
+    var whence = "E2"
+    var where = "E4"
+    println("Номер хода:$moveNumber\nИгрок сделал свой ход:\n - из $whence в $where\n")
+    moveNumber = 2
+    whence = "D2"
+    where = "D4"
+    println("Номер хода:$moveNumber\nИгрок сделал свой ход:\n - из $whence в $where\n")
+
+
+}
