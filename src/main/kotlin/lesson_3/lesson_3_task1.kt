860d35a1f91a9265b80666a2abce5f74c0ff957c
Evgeniy <E.poveshenko@gmail.com>
2023-07-17 18:31:32 +0300

    LK-3-1 print/

---
+++ src/main/kotlin/lesson_3/lesson_3_task1.kt
@@ -0,0 +1,10 @@
+package lesson_3
+
+fun main() {
+    val goodMorning = "Доброе Утро!"
+    val goodNight = "Добрый вечер!"
+    val userName = "Евгений"
+    println("$goodMorning $userName")
+    println("$goodNight $userName")
+
+}
