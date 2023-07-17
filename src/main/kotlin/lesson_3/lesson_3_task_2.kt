68e01bd85907876cfbc3ef846bb7d4b74512dc16
Evgeniy <E.poveshenko@gmail.com>
2023-07-17 18:56:22 +0300

    LK-3-2 print/

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val girlName = "Андреева Татьяна Сергеевна"
+    val age = "20"
+    val girlName2 = "Сидорова Татьяна Сергеевна"
+    val age2 = "22"
+    println("До момента как вышла замуж.\nФИО: $girlName \nВозраст: $age")
+    println()
+    println(("Вышла замуж.\nФИО: $girlName2 \nВозраст: $age2"))
+}
