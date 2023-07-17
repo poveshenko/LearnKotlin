042615c48ee668205b92c60e5c1bbd3c85187288
Evgeniy <E.poveshenko@gmail.com>
2023-07-17 19:32:01 +0300

    LK-3-3 print/

---
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -0,0 +1,20 @@
+package lesson_3
+
+fun main() {
+
+    val number = 5
+    val result = "Таблица умножения\n" +
+            "$number x 1 = ${number * 1}\n" +
+            "$number x 2 = ${number * 2}\n" +
+            "$number x 3 = ${number * 3}\n" +
+            "$number x 4 = ${number * 4}\n" +
+            "$number x 5 = ${number * 5}\n" +
+            "$number x 6 = ${number * 6}\n" +
+            "$number x 7 = ${number * 7}\n" +
+            "$number x 8 = ${number * 8}\n" +
+            "$number x 9 = ${number * 9}\n" +
+            "$number x 10 = ${number * 10}"
+    println(result)
+
+
+}
