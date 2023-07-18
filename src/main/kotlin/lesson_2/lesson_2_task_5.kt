1928d746eede2b501405ec1bb3502d666f5a283e
Evgeniy <E.poveshenko@gmail.com>
2023-07-18 18:01:39 +0300

    LK-2-5 print/

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,14 +1,10 @@
 package lesson_2
-
 import kotlin.math.pow
 
 fun main() {
-
     val clientInvested = 70_000
     val interestRate = 16.7
     val result = clientInvested * ((1 + (interestRate / 100)).pow(20))
     println(String.format("%.3f", result))
-
-
 }
 
