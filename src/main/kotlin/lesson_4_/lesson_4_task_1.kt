08e02d99e3998105648196394c39723c3834f9c8
Evgeniy <E.poveshenko@gmail.com>
2023-07-19 17:56:56 +0300

    LK-4-1 print/

---
+++ src/main/kotlin/lesson_4_/lesson_4_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_4_
+
+fun main() {
+
+    val today = TABLES == 13
+    val tomorrow = TABLES == 4
+    println("Доступность столиков на сегодня: $today\nДоступность столиков на сегодня: $tomorrow")
+}
+
+const val TABLES = 13
