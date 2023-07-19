795e57ee4336f3dd08a2a770052e7a508ef7b47a
Evgeniy <E.poveshenko@gmail.com>
2023-07-19 15:02:02 +0300

    LK-2-4 print/

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -4,9 +4,9 @@ fun main() {
     val buff = 20
     val oreCrystal = 7
     val ironOre = 11
-    val res = (oreCrystal * buff/100f).toInt()
-    val res2 = (ironOre * buff/100f).toInt()
-   println("Кристалическая Руда:  $res% бонусных")
-   println("Железная Руда:  $res2% бонусных")
+    val res = (oreCrystal * buff / 100f).toInt()
+    val res2 = (ironOre * buff / 100f).toInt()
+    println("Кристалическая Руда:  $res% бонусных")
+    println("Железная Руда:  $res2% бонусных")
 
 }
