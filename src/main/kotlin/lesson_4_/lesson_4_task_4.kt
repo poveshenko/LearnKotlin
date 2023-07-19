996226ff97ca187b9d9653cbe7e03007d455d6a2
Evgeniy <E.poveshenko@gmail.com>
2023-07-19 22:24:04 +0300

    LK-4-4 print/

---
+++ src/main/kotlin/lesson_4_/lesson_4_task_4.kt
@@ -0,0 +1,10 @@
+package lesson_4_
+
+fun main() {
+    val day = 1
+    val armTraining = "Упражнения для рук: ${1 == day}"
+    val legWorkout = "Упражнения для ног: ${2 == day}"
+    val backWorkout = "Упражнения для спины: ${3 == day}"
+    val pressTraining = "Упражнения для пресса: ${4 == day}"
+    println("$armTraining\n$legWorkout\n$backWorkout\n$pressTraining")
+}
