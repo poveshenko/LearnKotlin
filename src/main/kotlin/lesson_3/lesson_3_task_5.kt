fd8bfab63d0c3d67d106852573a02f177acbb41b
Evgeniy <E.poveshenko@gmail.com>
2023-07-20 22:05:08 +0300

    LK-3-5 print/Использовал массив/

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -1,12 +1,13 @@
 package lesson_3
 
 fun main() {
-    val player = "D2::D4::0"
-    val server: List<String> = player.split("::")
-    println("Сервер принял данные: $server")
+    println()
+    val server = arrayOf("D2", ";", "D4", "-", "0")
+    val text = "Сервер принял данные: "
+    println(text + server[0] + server[1] + server[2] + server[3] + server[4])
     val whence = server[0]
-    val where = server[1]
-    val moveNumber = server[2]
+    val where = server[2]
+    val moveNumber = server[4]
     println("Откуда: $whence")
     println("Куда: $where")
     println("Номер хода: $moveNumber")
