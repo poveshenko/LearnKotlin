a6e60b24f0a9fb2651aeb633a712908c5fd4ab3e
Evgeniy <E.poveshenko@gmail.com>
2023-07-20 22:14:45 +0300

    LK-3-5 print/Использовал массив/

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -3,7 +3,7 @@ package lesson_3
 fun main() {
     println()
     val server = arrayOf("D2", ";", "D4", "-", "0")
-    val text ="Сервер принял данные: "
+    val text = "Сервер принял данные: "
     println(text + server[0] + server[1] + server[2] + server[3] + server[4])
     val whence = server[0]
     val where = server[2]
