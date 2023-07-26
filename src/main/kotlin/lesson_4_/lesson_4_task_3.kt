5b98d1ad8bed74b8a77a4729765f5adf1edc0e10
Evgeniy <E.poveshenko@gmail.com>
2023-07-26 19:05:16 +0300

    LK-4-3 print/Изменение

--- src/main/kotlin/lesson_4_/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4_/lesson_4_task_3.kt
@@ -9,10 +9,11 @@ fun main() {
     val isSunny = true
     val isAwningOpen = true
     val airHumidityIs = 20
-    val currentSeason = "Не зима"
+    val currentSeason = "Зима"
 
     println("Погода сегодня: ${weatherToday == isSunny}")
     println("Тент на данный момент: ${awningAtTheMoment == isAwningOpen}")
     println("Влажность воздуха: ${airHumidityIs == airHumidity}")
-    println("Текущее время года: ${currentSeason == season}")
+    println("Текущее время года: ${currentSeason != season}")
 }
+
