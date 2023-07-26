d4aabce8c19d54b769671cd492d7d96ec1f24d2d
Evgeniy <E.poveshenko@gmail.com>
2023-07-26 18:53:40 +0300

    LK-3-5 print/Изменение

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -1,14 +1,10 @@
 package lesson_3
 
 fun main() {
-    println()
-    val server = arrayOf("D2", ";", "D4", "-", "0")
-    val text = "Сервер принял данные: "
-    println(text + server[0] + server[1] + server[2] + server[3] + server[4])
-    val whence = server[0]
-    val where = server[2]
-    val moveNumber = server[4]
-    println("Откуда: $whence")
-    println("Куда: $where")
-    println("Номер хода: $moveNumber")
+    val information = "D2-D4;0"
+    val whence = information.split("-", ";")
+    println("Сервер принял данные: $information")
+    println("Откуда: ${whence[0]}")
+    println("Куда: ${whence[1]}")
+    println("Номер хода: ${whence[2]}")
 }
