3648c8d5ffe2181eb8ffcfa891367ad15cb7b1ac
Evgeniy <E.poveshenko@gmail.com>
2023-07-26 13:57:15 +0300

    LK-4-2 print/исправление

--- src/main/kotlin/lesson_4_/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4_/lesson_4_task_2.kt
@@ -3,6 +3,12 @@ package lesson_4_
 fun main() {
     val cargoWithWeight = 50
     val volumeOfLiters = 100
-    println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': " +
-                "${(cargoWithWeight <= 35) && (cargoWithWeight >= 100) && (volumeOfLiters >= 100)}")
+    println(
+        "Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': " +
+                "${(cargoWithWeight <= averageWeightFrom) && (cargoWithWeight >= averageVolumeUpTo) && (volumeOfLiters >= averageVolume)}"
+    )
 }
+
+const val averageWeightFrom = 35
+const val averageVolumeUpTo = 100
+const val averageVolume = 100
