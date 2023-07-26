61cdef913e245a44d46126b965f4836852cf7b9e
Evgeniy <E.poveshenko@gmail.com>
2023-07-26 19:21:38 +0300

    LK-4-4 print/Изменение

--- src/main/kotlin/lesson_4_/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4_/lesson_4_task_4.kt
@@ -3,7 +3,7 @@ package lesson_4_
 fun main() {
     val numberOfDay = 1
     val isEven = numberOfDay % 2 == 0
-    val arms =  "Упражнения для рук: ${!isEven}"
+    val arms = "Упражнения для рук: ${!isEven}"
     val legs = "Упражнения для ног: $isEven"
     val back = "Упражнения для спины: $isEven"
     val press = "Упражнения для пресса: ${!isEven}"
