57a2b1fbdbc7d36c0173aa0b74d85b22f4a3e511
Evgeniy <E.poveshenko@gmail.com>
2023-07-27 21:34:25 +0300

    LK-6-2 print/ Изменение. Требуеться корректировка

--- src/main/kotlin/lesson_6/lesson_6_task_2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_2.kt
@@ -3,8 +3,9 @@ package lesson_6
 fun main() {
     println("Введите цифру")
     var second = readln().toInt()
-    while (second > 0) {
+    while (second>0) {
         Thread.sleep(1000)
-        println("Прошло ${second--} секунд")
+        println("${second--} секунд")
     }
+    println("Прошло $second секунд")
 }
