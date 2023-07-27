3c951bc41543a20898a1a0a2707704ac030721dd
Evgeniy <E.poveshenko@gmail.com>
2023-07-27 15:18:14 +0300

    LK-4-2 print/ Изменил константы

--- src/main/kotlin/lesson_4_/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4_/lesson_4_task_2.kt
@@ -5,10 +5,10 @@ fun main() {
     val volumeOfLiters = 100
     println(
         "Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': " +
-                "${(cargoWithWeight <= averageWeightFrom) && (cargoWithWeight >= averageVolumeUpTo) && (volumeOfLiters >= averageVolume)}"
+                "${(cargoWithWeight <= AVERAGE_WEIGHT_FROM) && (cargoWithWeight >= AVERAGE_VOLUME_UP_TO) && (volumeOfLiters >= AVERAGE_VOLUME)}"
     )
 }
 
-const val averageWeightFrom = 35
-const val averageVolumeUpTo = 100
-const val averageVolume = 100
+const val AVERAGE_WEIGHT_FROM = 35
+const val AVERAGE_VOLUME_UP_TO = 100
+const val AVERAGE_VOLUME = 100
