bc6012dbc84d99c3916d78f09059b3023a125c77
Evgeniy <E.poveshenko@gmail.com>
2023-07-27 15:50:53 +0300

    LK-5-3 print/

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -5,12 +5,12 @@ fun main() {
     val numberOne = readln().toInt()
     println("Введите второе число")
     val numberTwo = readln().toInt()
-    if (numberOne == winnerNumberOne && numberTwo == winnerNumberTwo)
+    if (numberOne == WINNER_NUMBER_ONE && numberTwo == WINNER_NUMBER_TWO)
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (numberOne == winnerNumberOne || numberTwo == winnerNumberTwo)
+    else if (numberOne == WINNER_NUMBER_ONE || numberTwo == WINNER_NUMBER_TWO)
         println("Вы выиграли утешительный приз!")
     else println("Неудача! Крутите барабан!")
 }
 
-const val winnerNumberOne = 5
-const val winnerNumberTwo = 10
+const val WINNER_NUMBER_ONE = 5
+const val WINNER_NUMBER_TWO = 10
