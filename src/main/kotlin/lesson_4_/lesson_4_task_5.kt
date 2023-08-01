92141580fb05b2197a41ad9d4f259d21004bac9c
Evgeniy <E.poveshenko@gmail.com>
2023-08-01 20:16:02 +0300

    LK-4-5 print/ Изменение на проверку

--- src/main/kotlin/lesson_4_/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4_/lesson_4_task_5.kt
@@ -2,34 +2,44 @@ package lesson_4_
 
 fun main() {
 
-    //Корабль
-    println("Имеет ли корабль повреждения? ")
-    val isShip = readln().toBoolean()
-    val commandOne = if (isShip) "-Есть"
-    else "-Нет"
+//    //Корабль
+//    println("Имеет ли корабль повреждения? ")
+//    val isDamage = readln().toBoolean()
+//    val commandOne = if (isDamage) "-Есть"
+//    else "-Нет"
+//
+//    //Погода
+//    println("Погода благоприятна или неблагоприятна? ")
+//    val isWeather = readln().toBoolean()
+//    val commandTwo = if (isWeather) "-Погода благоприятная"
+//    else "-Погода неблагоприятная"
+//
+//    //Провизия
+//    println("Укажите количество ящиков с провизией")
+//    val boxesOfProvisions = readln().toInt()
+//    val commandThree = if (boxesOfProvisions == NORMAL_BOXES) "-Провизии на борту коробля, достаточно! "
+//    else "-Не достаточно провизии!"
 
-    //Погода
-    println("Погода благоприятна или неблагоприятна? ")
+
+    println("Имеет ли корабль повреждения?")
+    val isDamage = readln().toBoolean()
+    println("Погода благоприятна или неблагоприятна?")
     val isWeather = readln().toBoolean()
-    val commandTwo = if (isWeather) "-Погода благоприятная"
-    else "-Погода неблагоприятная"
-
-    //Провизия
     println("Укажите количество ящиков с провизией")
     val boxesOfProvisions = readln().toInt()
-    val commandThree = if (boxesOfProvisions == NORMAL_BOXES) "-Провизии на борту коробля, достаточно! "
-    else "-Не достаточно провизии!"
-
-    //Экипаж
-    println("Укажите количество экипажа  на борту")
+    println("Укажите количесвто экипажа ")
     val crew = readln().toInt()
-    val commandFour = if (crew in CREW_FROM..CREW_UP_TO) "-Текущий состав экипажа в норме"
-    else "-Не достаточно людей на борту"
+    val result =
+        (boxesOfProvisions == NORMAL_BOXES) && (crew in CREW_FROM..CREW_UP_TO) || (crew == RECOMMENDED) && (isWeather) && (isDamage)
+
+    println(result)
 
     //Результат
-    println("Научно-исследовательский корабль:\n$commandOne\n$commandTwo\n$commandThree\n$commandFour\n")
+//    println("Научно-исследовательский корабль:\n$commandOne\n$commandTwo\n$commandThree\n$commandFour\n")
 }
 
 const val CREW_FROM = 55
 const val CREW_UP_TO = 70
+const val RECOMMENDED = 70
 const val NORMAL_BOXES = 50
+
