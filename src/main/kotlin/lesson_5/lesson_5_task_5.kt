4e8064c6bc89b8a1ad7183e3e58151652fdab60f
Evgeniy <E.poveshenko@gmail.com>
2023-08-01 21:09:59 +0300

    LK-5-3 print/ Изменение метода рандом/Пробелы и пояснение для читабельности / Проверка в обратном порядке

--- src/main/kotlin/lesson_5/lesson_5_task_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -4,20 +4,23 @@ import java.util.*
 
 fun main() {
     //Генератор случайных чисел
-    val random = Random()
-    val winnerNumberTwo = random.nextInt(100)
-    val winnerNumberONe = random.nextInt(100)
+
+    val winnerNumberTwo = Random().nextInt(100)
+    val winnerNumberONe = Random().nextInt(100)
+
     //Ввод данных
     println("Введите число")
     val numberOne = readln().toInt()
     println("Введите второе число")
     val numberTwo = readln().toInt()
-    if (numberOne == winnerNumberONe && numberTwo == winnerNumberTwo)
+
+    // Сравнение переменных
+    if (numberOne == winnerNumberONe && numberTwo == winnerNumberTwo || numberOne == winnerNumberTwo && numberTwo == winnerNumberONe)
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (numberOne == winnerNumberONe || numberTwo == winnerNumberTwo)
+    else if (numberOne == winnerNumberONe || numberTwo == winnerNumberTwo || numberTwo == winnerNumberONe || numberOne == winnerNumberTwo)
         println("Вы выиграли утешительный приз!")
     else println("Неудача! Крутите барабан!")
+
     // Результат
     println("Выйгрышные числа: $winnerNumberONe и $winnerNumberTwo")
-
 }
