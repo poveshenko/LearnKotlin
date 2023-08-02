61be650a0f237c99f15dddffd5ebfbcb06071369
Evgeniy <E.poveshenko@gmail.com>
2023-08-02 21:35:36 +0300

    LK-5-1 print/ Изменение/

--- src/main/kotlin/lesson_5/Lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/Lesson_5_task_1.kt
@@ -2,10 +2,13 @@ package lesson_5
 
 fun main() {
     println("Докажите что вы не БОТ\nСложите 2 числа:")
-    val number = readln().toInt()
-    val numberTwo = readln().toInt()
-    when (number) {
-        +numberTwo -> println("Добро пожаловать!")
+    val number = (1..9).random()
+    val numberTwo = (1..9).random()
+    println("${number}+${numberTwo}=")
+    val unswer = readln().toInt()
+    when (unswer) {
+        number + numberTwo ->
+            println("Добро пожаловать! ")
         else -> println("Доступ запрещен!")
     }
 }
