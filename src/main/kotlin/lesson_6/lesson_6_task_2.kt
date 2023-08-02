601dc42709a4c720a46471053303c844109b17de
Evgeniy <E.poveshenko@gmail.com>
2023-08-02 21:39:22 +0300

    LK-6-2 print/

--- src/main/kotlin/lesson_6/lesson_6_task_2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_2.kt
@@ -3,7 +3,7 @@ package lesson_6
 fun main() {
     println("Введите цифру")
     var second = readln().toInt()
-    while (second>0) {
+    while (second > 0) {
         Thread.sleep(1000)
         println("${second--} секунд")
     }
