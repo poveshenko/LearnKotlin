9dcf58e653a021b984a4e4f9e8301832ddf7b3e1
Evgeniy <E.poveshenko@gmail.com>
2023-08-02 14:36:47 +0300

    LK-5-3 print/ Изменение - пробелы

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -7,7 +7,7 @@ fun main() {
     val numberTwo = readln().toInt()
     if (numberOne == WINNER_NUMBER_ONE && numberTwo == WINNER_NUMBER_TWO || numberOne == WINNER_NUMBER_TWO && numberTwo == WINNER_NUMBER_ONE)
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (numberOne == WINNER_NUMBER_ONE || numberTwo == WINNER_NUMBER_TWO||numberTwo==WINNER_NUMBER_ONE||numberOne==WINNER_NUMBER_TWO)
+    else if (numberOne == WINNER_NUMBER_ONE || numberTwo == WINNER_NUMBER_TWO || numberTwo == WINNER_NUMBER_ONE || numberOne == WINNER_NUMBER_TWO)
         println("Вы выиграли утешительный приз!")
     else println("Неудача! Крутите барабан!")
 }
