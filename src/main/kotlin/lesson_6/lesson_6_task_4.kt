21067d977103393719b5e954a2bb7227b3c4d37d
Evgeniy <E.poveshenko@gmail.com>
2023-08-02 22:27:52 +0300

    LK-6-2 print/изменение "--count"

--- src/main/kotlin/lesson_6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -2,14 +2,16 @@ package lesson_6
 
 
 fun main() {
+    val randomNumber = (1..9).random()
     var count = 5
-    val n = (1..9).random()
     while (count >= 0) {
         val number = readln().toInt()
-        if (number == n) {
+        if (number == randomNumber) {
             println("Это велеколепная игра!")
             break
-        } else println("Не верно, осталось ${count--} попыток")
+        } else println("Не верно, осталось ${--count} попыток")
     }
-    println("\nБыло загадано число $n")
+    println("\nБыло загадано число $randomNumber")
 }
+
+
