4b5b11ed2786c7c585d6bc4d92039156f6d410b8
Evgeniy <E.poveshenko@gmail.com>
2023-08-03 14:47:59 +0300

    LK-7-1 print/Шестизначные пороль

---
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -0,0 +1,23 @@
+package lesson_7
+
+fun main() {
+    //Например, k5p6m2.
+
+    val simOne = 'k'
+    val simTwo = 'p'
+    val simThree = 'm'
+
+    val one = (2..6).random()
+    val two = (2..6).random()
+    val three = (2..6).random()
+
+    println("$simOne$one$simTwo$two$simThree$three")
+//    var str = "kpm562"
+//    var password = ""
+//    for (i in 1..6) {
+//        password += str.random()
+//    }
+//    println(password)
+}
+
+
