666f89bb5c7d382949dc870f3f5c3feaa6e64e7d
Evgeniy <E.poveshenko@gmail.com>
2023-08-03 16:01:38 +0300

    LK-7-1 print/Авторизация по коду

---
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_7
+
+import kotlin.random.Random
+fun main(){
+do {
+    val generatorKey = Random.nextInt(1000, 9999)
+    println("Ваш код авторизации: $generatorKey\nВведите код ")
+    val number = readln().toInt()
+} while (number != generatorKey)
+println("Добро пожаловать!")
+}
+
+
+
+
+
+
