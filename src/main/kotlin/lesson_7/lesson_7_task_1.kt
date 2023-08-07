5995142a991d89fbdd7ae95aa39de0a7dd6c8132
Evgeniy <E.poveshenko@gmail.com>
2023-08-07 19:42:13 +0300

    LK-7-1 print/ Изменение -

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -1,22 +1,16 @@
 package lesson_7
 
 fun main() {
-    //Например, k5p6m2.
-    val simOne = 'k'
-    val simTwo = 'p'
-    val simThree = 'm'
 
-    val one = (2..6).random()
-    val two = (2..6).random()
-    val three = (2..6).random()
-
-    println("$simOne$one$simTwo$two$simThree$three")
-//    var str = "kpm562"
-//    var password = ""
-//    for (i in 1..6) {
-//        password += str.random()
-//    }
-//    println(password)
+    val letter = 'a'..'z'
+    val number = 2..6
+    var password = ""
+    val generator = letter + number
+    for (i in 1..6) {
+        password += generator.random()
+    }
+    println(password)
 }
+
 
 
