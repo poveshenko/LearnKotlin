637bb7e9404bb0fd6cd343c4271c4d4538479c7b
Evgeniy <E.poveshenko@gmail.com>
2023-08-07 18:59:33 +0300

    LK-8-1 print/ Изменение -

--- src/main/kotlin/lesson_7/lesson_7_task_2.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -1,12 +1,16 @@
 package lesson_7
+
 import kotlin.random.Random
-fun main(){
-do {
-    val generatorKey = Random.nextInt(1000, 9999)
-    println("Ваш код авторизации: $generatorKey\nВведите код ")
-    val number = readln().toInt()
-} while (number != generatorKey)
-println("Добро пожаловать!")
+
+fun main() {
+
+    do {
+        val generatorKey = Random.nextInt(1000, 9999)
+        println("Ваш код авторизации: $generatorKey\nВведите код ")
+        val number = readln().toInt()
+    } while (number != generatorKey)
+
+    println("Добро пожаловать!")
 }
 
 
