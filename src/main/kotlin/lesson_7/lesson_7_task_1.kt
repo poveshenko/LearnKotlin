9760b47fa5ac26f6bc0a92462ec7e417fa3ebad0
Evgeniy <E.poveshenko@gmail.com>
2023-08-08 20:31:37 +0300

    LK-7-1 print/ Изменение -

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -2,10 +2,14 @@ package lesson_7
 
 fun main() {
 
-    val letter = 'a'..'z'
-    val number = 2..6
+    val randomLetter = 'a'..'z'
+    val letterOne = randomLetter
+    val letterTwo = randomLetter
+    val letterThree = randomLetter
+
+    val number = 0..9
     var password = ""
-    val generator = letter + number
+    val generator = number+letterOne+letterTwo+letterThree
     for (i in 1..6) {
         password += generator.random()
     }
