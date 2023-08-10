97287b3eb89432ea542a540a03b52e24d28c3609
Evgeniy <E.poveshenko@gmail.com>
2023-08-10 13:38:20 +0300

    LK-8-2 print/Изменение в коде

--- src/main/kotlin/lesson_8/lesson_8_task_2.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -2,18 +2,13 @@ package lesson_8
 
 fun main() {
     val recipeOfBorsch = arrayOf("Свекла", "Морковь", "Лук", "Картошка", "Томатная паста", "Честнок", "Говядина", "Соль")
-    var found = false
     println("Что вы желаете найти? ")
     val find = readln()
     for (n in recipeOfBorsch) {
         if (n == find) {
-            found = true
-            break
+            println("Ингредиент: [$find] в рецепте есть.")
+            return
         }
     }
-    if (found) {
-        println("Ингредиент: [$find] в рецепте есть.")
-    } else {
-        println("Такого ингредиента в рецепте нет.")
-    }
+    println("Такого ингредиента в рецепте нет.")
 }
