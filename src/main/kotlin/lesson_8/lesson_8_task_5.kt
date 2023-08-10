34990ac98d2522bc827fc3e89b79e398a8daf4eb
Evgeniy <E.poveshenko@gmail.com>
2023-08-10 13:42:15 +0300

    LK-8-2 print/Размер и заполнение массива через ввод

---
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -0,0 +1,16 @@
+package lesson_8
+
+fun main(){
+    println("Введите Количество ингридиентов:")
+    val quantityOfIngredients = readln().toInt()
+    val recipeOfBorsch = Array(quantityOfIngredients) { "" }
+    println("Введите ингридиенты:")
+    for (i in 0..<quantityOfIngredients) {
+        recipeOfBorsch[i] = readln()
+    }
+    // Вывод массива
+    println("Список ингридиентов")
+    for (element in recipeOfBorsch) {
+        println(println("Ингридиенты:${recipeOfBorsch.indexOf(element) + 1}: $element"))
+    }
+}
