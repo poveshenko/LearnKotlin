f117f9d5c143f6a8329b5dee094837da553ef8d9
Evgeniy <E.poveshenko@gmail.com>
2023-08-15 22:27:08 +0300

    LK-9-2 print/Список ингредиентов/+1 к списку

---
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -0,0 +1,27 @@
+package lesson_9
+
+fun main() {
+    val ingredients = mutableListOf("мука", "яйцо", "молоко")
+
+    println("В рецепте есть базовые ингредиенты:\n\n[базовый_список_ингредиентов]")
+    ingredients.forEach {
+        Thread.sleep(1000)
+        println("Ингридиент: ${ingredients.indexOf(it) + 1} - $it ")
+    }
+    Thread.sleep(2000)
+    println("\nЖелаете добавить еще?")
+    val user = readln()
+    if (user.equals("Да", ignoreCase = true)) {
+        println("Какой ингредиент вы хотите добавить?")
+    } else if (user.equals("Нет", ignoreCase = true)) {
+        print("Завершение программы")
+        return
+    }
+    val newAddIngredient = readln()
+    ingredients.add(newAddIngredient)
+    println("Теперь в рецепте есть следующие ингредиенты:\n\n[список_ингредиентов]")
+    ingredients.forEach {
+        Thread.sleep(1000)
+        println("Ингридиент: ${ingredients.indexOf(it) + 1} - $it ")
+    }
+}
