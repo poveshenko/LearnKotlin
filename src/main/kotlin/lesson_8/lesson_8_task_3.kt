70880f0bca486cf88645ffb6aa82a9556bc58584
Evgeniy <E.poveshenko@gmail.com>
2023-08-15 17:53:44 +0300

    LK-8-3 print/Изменение

--- src/main/kotlin/lesson_8/lesson_8_task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_3.kt
@@ -1,22 +1,9 @@
 package lesson_8
 
 fun main() {
-//    val recipeOfBorsch =
-//        arrayOf("Свекла", "Морковь", "Лук", "Картошка", "Томатная паста", "Честнок", "Говядина", "Соль")
-//    println("Что вы желаете найти? ")
-//
-//    var x = false;
-//    for (i in recipeOfBorsch) {
-//        val find = readln()
-//i.contains(find)
-//    println("Ингредиент: [$i] в рецепте есть.")
-//
-//}var find = readln()
-    val arr = arrayOf("apple", "banana", "cherry")
+    val recipeOfBorsch =
+        arrayOf("Свекла", "Морковь", "Лук", "Картошка", "Томатная паста", "Честнок", "Говядина", "Соль")
+    println("Что вы желаете найти? ")
     val find = readln()
-    for (x in arr) {
-        println("Ингредиент: [${find == x}]  в рецепте есть.")
-        break
-    }
+    println(recipeOfBorsch.contains(find))
 }
-
