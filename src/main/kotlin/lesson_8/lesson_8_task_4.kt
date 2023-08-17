b6825013b3a266393eb0a638734e5abbf9370d25
Evgeniy <E.poveshenko@gmail.com>
2023-08-17 17:04:08 +0300

    LK-8-4 print/Изменение

--- src/main/kotlin/lesson_8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -1,7 +1,8 @@
 package lesson_8
 
 fun main() {
-    val recipeOfBorsch = arrayOf("Свекла", "Морковь", "Лук", "Картошка", "Томатная паста", "Чеснок", "Говядина", "Соль")
+    val recipeOfBorsch =
+        arrayOf("Свекла", "Морковь", "Лук", "Картошка", "Томатная паста", "Чеснок", "Говядина", "Соль")
 
     println("Список ингредиентов:")
     for (ingredient in recipeOfBorsch) {
@@ -11,17 +12,15 @@ fun main() {
     println("Введите ингредиент, который вы хотите заменить:")
     val findIngredient = readln()
     if (findIngredient !in recipeOfBorsch) {
-        println("Ингредиента \"$findIngredient\"} нет в списке.")
+        println("Ингредиента \"$findIngredient\" нет в списке.")
     } else {
         println("Введите замену для ингредиента $findIngredient:")
         val newIngredient = readln()
 
         // Заменяем ингредиент
-        for (index in recipeOfBorsch.indices) {
-            if (recipeOfBorsch[index] == findIngredient) {
-                recipeOfBorsch[index] = newIngredient
-            }
-        }
+        val index = recipeOfBorsch.indexOf(findIngredient)
+        recipeOfBorsch[index] = newIngredient
+
         println("Готово! Вы сохранили следующий список: ")
         for (ingredient in recipeOfBorsch) {
             println("Ингредиент ${recipeOfBorsch.indexOf(ingredient) + 1} $ingredient")
