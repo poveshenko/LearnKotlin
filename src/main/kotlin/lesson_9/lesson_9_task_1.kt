914bff4f770e7b245dd08689f19e4ea2789dce4a
Evgeniy <E.poveshenko@gmail.com>
2023-08-17 17:07:59 +0300

    LK-9-1 print/Изменение

--- src/main/kotlin/lesson_9/lesson_9_task_1.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -1,9 +1,9 @@
 package lesson_9
 
 fun main() {
-    val saladRecipe = listOf<String>("Помидор", "Огурец", "Лук", "Зелень")
-    println("В рецепте есть следующие ингредиенты:\n[список_ингредиентов]")
-    saladRecipe.forEach() {
+    val saladRecipe = listOf("Помидор", "Огурец", "Лук", "Зелень")
+    println("В рецепте есть следующие ингредиенты:")
+    saladRecipe.forEach {
         println("Ингридиент: ${saladRecipe.indexOf(it) + 1} $it ")
     }
 }
