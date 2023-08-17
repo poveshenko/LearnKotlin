55854ce12760e03b22449ae0c15bdcce4c4862c2
Evgeniy <E.poveshenko@gmail.com>
2023-08-17 18:15:23 +0300

    LK-9-3 print/Изменение кода/

--- src/main/kotlin/lesson_9/lesson_9_task_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -1,15 +1,16 @@
 package lesson_9
 
 fun main() {
-    val omelette = listOf("Яицо", "Молоко", "Масло")
-    val omelette2 = listOf(2, 50, 15)
+
+    val omelette = listOf("Яйцо", "Молоко", "Масло")
+    val omeletteOnePortion = listOf(2, 50, 15)
 
     println("Введите количество порций?")
     val countPortion = readln().toInt()
     println("На [$countPortion] порций вам понадобится:")
 
-    omelette.forEachIndexed() { addPortion, ingredient ->
-        val portion = omelette2.getOrNull(addPortion)
-        println("Ингридиент: $ingredient | Количество - ${portion!! * countPortion}")
+    val portion = omeletteOnePortion.map { it * countPortion }
+    portion.forEachIndexed { index, addCount ->
+        println("Ингредиент: ${omelette[index]} | Количество: $addCount")
     }
 }
