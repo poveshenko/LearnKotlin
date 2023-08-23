51594e54ceaefc5497c1a875d95f84b5f2cc4929
Evgeniy <E.poveshenko@gmail.com>
2023-08-23 17:29:51 +0300

    LK-10-1 print/ Игра в кости

---
+++ src/main/kotlin/lesson_10/lesson_10_task_1.kt
@@ -0,0 +1,26 @@
+package lesson_10
+
+fun main() {
+    result(computer = computerTurn(), player = playerTurn())
+}
+
+fun playerTurn(): Int {
+    val actionPlayer = (1..6).random()
+    println("Ход игрока: $actionPlayer")
+    return actionPlayer
+}
+
+fun computerTurn(): Int {
+    val actionComputer = (1..6).random()
+    println("Ход компьютера: $actionComputer")
+    return actionComputer
+
+}
+
+fun result(computer: Int, player: Int) {
+    if (computer > player) {
+        println("Победила машина")
+    } else {
+        println("Победило человечество")
+    }
+}
