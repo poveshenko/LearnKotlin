292d35a94455c8628a5c9c0544095e57f11fff5e
Evgeniy <E.poveshenko@gmail.com>
2023-08-23 14:57:20 +0300

    LK-9-4 print/исправление

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -2,13 +2,10 @@ package lesson_9
 
 fun main() {
     println("Введите пять названий ингредиентов блюда, разделяя их запятыми:")
-
-    val ingredientsStr = readln() // считываем строку с ингредиентами
-    val ingredientsList = ingredientsStr.split(",").toList() // разбиваем строку на список ингредиентов
-    //-----
-    val sortedIngredients = ingredientsList.sorted() // сортируем список ингредиентов по алфавиту
+    val ingredientsStr = readln()
+    val sortedIngredients = ingredientsStr.split(",").sorted()
     println("Отсортированный список ингредиентов:")
     sortedIngredients.forEach {
         println(" Ингридиент: (${sortedIngredients.indexOf(it) + 1}) - $it")
-    } // выводим отсортированный список ингредиентов
+    }
 }
