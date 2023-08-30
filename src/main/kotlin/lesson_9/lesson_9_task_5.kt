ee02b3ac1988ae5033df0eb5e414259038e41f3b
Evgeniy <E.poveshenko@gmail.com>
2023-08-30 15:03:32 +0300

    9-5/print Исправлен

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -2,22 +2,14 @@ package lesson_9
 
 fun main() {
     val ingredients = mutableSetOf<String>()
-    while (ingredients.size < 5) {
+    repeat(5) {
         print("Введите название ингредиента: ")
-        val ingredient = readln()
-        ingredients.add(ingredient)
+        ingredients.add(readln())
     }
-    val sortedIngredients = ingredients.sorted()
-    println(sortedIngredients.joinToString(", ") + ".")
+
+    val formattedIngredients = ingredients.sorted().mapIndexed { index, ingredient ->
+        if (index == 0) ingredient.replaceFirstChar(Char::uppercase) else ingredient
+    }
+
+    println(formattedIngredients.joinToString(", ") + ".")
 }
-//    sortedIngredients[1].capitalize()
-//
-//    println(sortedIngredients.joinToString(", ") + ".")
-//}
-//Нужно еще немного доработать)
-//
-//1.результат должен повторить повторы - для пробы поиска коллекции, которая не умеет хранить повторы и
-//делать приведение, подобное ей.
-//2.Чтобы сделать первый компонент заглавной буквы, достаточно сначала обратиться к нему по индексу,
-//затем либо по принципу многомерного массива [][] также взять строки первого элемента, либо сначала вызвать для
-//него()
