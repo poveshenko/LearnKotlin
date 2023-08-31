e2dbbea9146185e67bd41ef0da51542ea77ec4af
Evgeniy <E.poveshenko@gmail.com>
2023-08-31 23:04:31 +0300

    12-2//print Конструктор - погода //часть2

---
+++ src/main/kotlin/lesson_12/lesson_12_task_2.kt
@@ -0,0 +1,27 @@
+package lesson_12
+
+
+class Weather(
+    var temperatureNight: Int = 10,
+    var temperatureDay: Int = 20,
+    var isRain: Boolean = false
+
+) {
+
+    fun info() {
+        println("Погода на сегодня:\n1.Ночь: $temperatureNight °C\n2.День: $temperatureDay °C\n3.Был ли дождь: $isRain\n")
+    }
+
+
+}
+
+fun main() {
+    val monday = Weather()
+    monday.info()
+
+    val tuesday = Weather()
+    tuesday.info()
+
+    val wednesday = Weather()
+    wednesday.info()
+}
