0b52eb9b265bf58c17e5da3022eb6ad49b7c7674
Evgeniy <E.poveshenko@gmail.com>
2023-09-04 16:09:49 +0300

    12-2//print//Исправленно

--- src/main/kotlin/lesson_12/lesson_12_task_2.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_2.kt
@@ -5,14 +5,11 @@ class Weather(
     var temperatureNight: Int = 10,
     var temperatureDay: Int = 20,
     var isRain: Boolean = false
-
 ) {
 
     fun info() {
         println("Погода на сегодня:\n1.Ночь: $temperatureNight °C\n2.День: $temperatureDay °C\n3.Был ли дождь: $isRain\n")
     }
-
-
 }
 
 fun main() {
