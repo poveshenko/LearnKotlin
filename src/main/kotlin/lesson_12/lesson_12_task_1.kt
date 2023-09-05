997495140ebd7e4b5337b68e93efaf8cfac5b572
Evgeniy <E.poveshenko@gmail.com>
2023-09-05 22:03:46 +0300

    12-1//print//Исправленно

--- src/main/kotlin/lesson_12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -6,23 +6,23 @@ class Weather {
     var temperatureDay = 20
     var isRain = false
 
-    fun info() {
+    fun printInfo() {
         println("Погода на сегодня:\n1.Ночь: $temperatureNight °C\n2.День: $temperatureDay °C\n3.Был ли дождь: $isRain\n")
     }
 }
 
 fun main() {
     val monday = Weather()
-    monday.info()
+    monday.printInfo()
     monday.temperatureNight = 8
     monday.temperatureDay = 25
     monday.isRain = false
-    monday.info()
+    monday.printInfo()
 
     val tuesday = Weather()
-    tuesday.info()
+    tuesday.printInfo()
     tuesday.temperatureNight = 7
     tuesday.temperatureDay = 28
     tuesday.isRain = false
-    tuesday.info()
+    tuesday.printInfo()
 }
