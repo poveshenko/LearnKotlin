945c44437cdaf7cd7a5a4c162c94ed69e1f9f721
Evgeniy <E.poveshenko@gmail.com>
2023-09-06 17:33:24 +0300

    13-3//print//Телефонный справочник - часть 3

---
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -0,0 +1,25 @@
+package lesson_13
+
+class ContactPart3(var name: String? = null, val number: Long? = null, val company: String? = null) {
+
+    fun printInfo() {
+        val _company = company ?: "[не указано]"
+        val _number = number  ?: "[не указано]"
+        val _name = name ?: "[не указано]"
+
+
+        println("Имя: $_name\nНомер: $_number\nКомпания: $_company\n")
+    }
+}
+
+fun main() {
+
+    val contact = listOf(
+        ContactPart3("Ростислав",89123456789,"Компания"),
+        ContactPart3()
+    )
+
+    contact.forEach() {
+        it.printInfo()
+    }
+}
