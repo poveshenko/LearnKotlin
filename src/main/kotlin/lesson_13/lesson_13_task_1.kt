a3e6b12ad1c19e9d5401f91deb05442c5fb8183e
Evgeniy <E.poveshenko@gmail.com>
2023-09-06 16:30:38 +0300

    13-1//print//Исправленно

--- src/main/kotlin/lesson_13/lesson_13_task_1.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_1.kt
@@ -1,17 +1,11 @@
 package lesson_13
 
-class phoneBook(
-    val name: String,
-    val number: Long,
-    val company: String? = null
-) {
+class Contact(val name: String, val number: Int, val company: String?)
 
-}
-fun main(){
-    val phone = phoneBook("nokia",79785265897)
-    println(phone.name)
-    println(phone.number)
-    println(phone.company)
-
-
+fun main() {
+    // val contact = Contact("Валера", 123456789, "Компания 1")
+    val contact = Contact("Петр", 987654321, null)
+    println(contact.name)
+    println(contact.number)
+    println(contact.company)
 }
