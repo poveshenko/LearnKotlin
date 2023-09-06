d759bafb88d6d310e781a3c29f0a061357fb8675
Evgeniy <E.poveshenko@gmail.com>
2023-09-06 17:36:30 +0300

    13-2//print//Телефонный справочник - часть 2 / исправленно

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -3,11 +3,12 @@ package lesson_13
 class ContactPart2(val name: String, val number: Long, val company: String? = null) {
 
     fun printInfo() {
-        println("Имя: $name\nНомер: $number\nКомпания: $company")
+        val _company = company ?: "не указано"
+        println("Имя: $name\nНомер: $number\nКомпания: $_company")
     }
 }
 
 fun main() {
-    val contact = ContactPart2("Ростислав", 89123456789, "не указано")
+    val contact = ContactPart2("Ростислав", 89123456789)
     contact.printInfo()
 }
