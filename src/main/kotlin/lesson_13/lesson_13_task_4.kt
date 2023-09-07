5d76d3caa1bc887d0978d8f69ff43a83f8741ace
Evgeniy <E.poveshenko@gmail.com>
2023-09-07 18:17:55 +0300

    13-4//print//Телефонный справочник - часть 4/исправленно

--- src/main/kotlin/lesson_13/lesson_13_task_4.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_4.kt
@@ -10,38 +10,33 @@ class ContactPart4(var name: String? = null, val number: Long? = null, val compa
         println("Имя: $_name\nНомер: $_number\nКомпания: $_company\n")
     }
 
-    fun usefulFun(name: String? = null, number: Long, company: String? = null) {
-
+    fun info(name: String? = null, number: Long?, company: String? = null) {
         println("Новый контакт:\nИмя: $name\nНомер: $number\nКомпания: $company\n")
     }
 }
 
-    fun main() {
-        val contact = ContactPart4()
-        contact.printInfo()
-
-        while (true) {
-            println("Если хотите добавить новый  контакт, введите \"да\"")
-            val x = readln()
-            if (x.equals("да", ignoreCase = true)) {
-                println("Введите имя:")
-                val newName = readln()
-                println("Введите номер:")
-                val newNumber = readln().toLong()
-                println("Введите компанию:")
-                val newCompany = readln()
-                contact.usefulFun(newName, newNumber, newCompany)
-            } else {
-                break
+fun main() {
+    val contact = ContactPart4()
+    contact.printInfo()
+    while (true) {
+        println("Если хотите добавить новый  контакт, введите \"да\"")
+        val action = readln()
+        if (action.equals("да", ignoreCase = true)) {
+            println("Введите имя:")
+            var newName = readln()
+            if (newName == "") {
+                newName = "null"
             }
+            println("Введите номер:")
+            val newNumber = readln().toLong()
+            println("Введите компанию:")
+            var newCompany = readln()
+            if (newCompany == "") {
+                newCompany = "null"
+            }
+            contact.info(newName , newNumber, newCompany)
+        } else {
+            break
         }
     }
-
-//    val contact = listOf(
-//        ContactPart3("Ростислав", 89123456789, "Компания"),
-//        ContactPart3()
-//    )
-//
-//    contact.forEach() {
-//        it.printInfo()
-//    }
+}
